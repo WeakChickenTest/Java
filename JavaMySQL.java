365eb870882692f0b66375f1ca244e9e5f39c1fd
WenJinGuo <WenJinGuo@China.cn>
2022-04-04 20:20:54 +0800

    查询MySQL数据库

--- JavaMySQL.java
+++ JavaMySQL.java
@@ -5,7 +5,9 @@
 import java.sql.*;
 
 public class JavaMySQL {
-    Connection con;
+    static Connection con;
+    static Statement sql;
+    static ResultSet res;
 
     public Connection getConnection() {
         try {// try和catch成对出现
@@ -14,8 +16,8 @@ public class JavaMySQL {
             e.printStackTrace();
         }
         try {
-            con = DriverManager.getConnection("jdbc:mysql://192.168.0.110:3306/Test","root","12345678");// 数据库的地址、远程连接用户名和密码
-            System.out.println("数据库连接成功！");
+            // 数据库的地址、远程连接用户名和密码
+            con = DriverManager.getConnection("jdbc:mysql://192.168.0.139:3306/信息库","root","123456");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -23,6 +25,22 @@ public class JavaMySQL {
     }
     public static void main(String[] args) {
         JavaMySQL c = new JavaMySQL();
-        c.getConnection();
+        con = c.getConnection();
+        try {
+            sql = con.createStatement();
+            res = sql.executeQuery("select * from tb_stu");
+            while (res.next()) {
+                String id = res.getString("id");
+                String name = res.getString("name");
+                String sex = res.getString("sex");
+                String birthday = res.getString("birthday");
+                System.out.print("编号:" + id + "\t");
+                System.out.print("姓名:" + name + "\t");
+                System.out.print("性别:" + sex + "\t");
+                System.out.println("出生日期:" + birthday);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
