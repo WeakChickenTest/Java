eebab11a4ef0c299587590ea37108bf086eef719
WenJinGuo <WenJinGuo@China.cn>
2022-04-05 22:31:00 +0800

    把汉字改成拼音

--- JiuJiuChengFaBiao.java
+++ JiuJiuChengFaBiao.java
@@ -1,8 +1,8 @@
 /**
  * @Author WenJinGuo
- * @create 2020/12/29 21:19
+ * @reate 2020/12/29 21:19
  */
-public class 九九乘法表 {
+public class JiuJiuChengFaBiao {
     public static void main(String[] args) {
         for(int a = 1;a<=9;a++){
             for(int b = 1;b<=a;b++){
